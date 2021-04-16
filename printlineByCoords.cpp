fb31adc3682ea65ee44dd4d521bdbdf0195a0e9f
amarteyfio <77846610+amarteyfio@users.noreply.github.com>
2021-04-16 23:45:26 +0000

    Update printlineByCoords.cpp

--- printlineByCoords.cpp
+++ printlineByCoords.cpp
@@ -1,4 +1,17 @@
 void printLineByCoords(LineId lid, Line linesArray[], const
 int MaxLnsSize, Point pointsArray[], const int MaxPntsSize){
+    for(int x1; x1 < MaxPntsSize; x1++){
+        for(int x2;x2 < MaxPntsSize; x2++;){
+            for(int y1; y1 < MaxPntsSize; y1 ++){
+                for(int y2; y2 < MaxPntsSize; y2++){
+                    linesArray[MaxLnsSize] >> {"("+ x1 + "," + y1 + "," + x2 + "," + y2 +")" };
+                    cout << linesArray[MaxLnsSize]
+                }
+
+            }
+
+        }
+         
+    }
     
 }
