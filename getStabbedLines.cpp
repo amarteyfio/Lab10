c1e2362c370ccb7666e186f19b635f08ace31d3b
amarteyfio <77846610+amarteyfio@users.noreply.github.com>
2021-04-16 23:19:17 +0000

    Add files via upload

---
+++ getStabbedLines.cpp
@@ -0,0 +1,6 @@
+void getStabbedLines (const int xcoord, Line linesArray[], const
+int MaxLnsSize, const int NumLines, Point pointsArray[],
+const int MaxPtsSize, Line stabbedLines[],
+const int MaxStbSize, int& NumOfStbLines){
+    
+}
---
+++ printlineByCoords.cpp
@@ -0,0 +1,4 @@
+void printLineByCoords(LineId lid, Line linesArray[], const
+int MaxLnsSize, Point pointsArray[], const int MaxPntsSize){
+    
+}
